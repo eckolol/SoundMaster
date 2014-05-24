23565b3832b55806cf1018e3b893b107df6855db
Jerome Botineau <jeromebotineau@gmail.com>
2014-05-25 01:08:56 +0200

    Add playlist

--- app.js
+++ app.js
@@ -14,16 +14,41 @@ var server = app.listen(1337);
 
 var io = require('socket.io').listen(server);
 
-io.sockets.on('connection', function (socket) {
-  socket.on('playStream', function (sound){
+var playlist = [];
 
+io.sockets.on('connection', function(socket) {
+  socket.on('playStream', function(sound){
+    var track = '';
     if(sound.type == 'youtube'){
       track = encodeURI('http://www.youtube.com/watch?v=' + sound.stream);
     }
 
-    vlc.status.play(track,function () {
-      io.sockets.emit('played');
-    });
+    playlist.push(track);
+    console.log('Add new Song');
+    playNextSong();
+    io.sockets.emit('done');
 
   });
 });
+
+function playNextSong () {
+  var track = playlist[0];
+  vlc.request('status',function(err, data) {
+
+    if (err) throw new Error('Please Start Vlc');
+
+    if (!data.position && data.state === 'stopped' && typeof track !=='undefined') {
+      console.log('start new Song');
+      console.log(track);
+      vlc.status.play(track,function(){
+        playlist.shift();
+      });
+    }
+
+  });
+}
+
+
+setInterval(function main () {
+  playNextSong();
+}, 5000);
